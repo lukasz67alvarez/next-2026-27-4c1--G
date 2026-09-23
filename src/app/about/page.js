1996d008782172af0b93bc9ccc426f13ff793527
lukasz67alvarez <lukaszgrzebien236@gmail.com>
2026-09-23 09:01:28 +0200

    strona

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main style={{ padding: '2rem' }}>
+      <h1>O nas</h1>
+      <p>Dowiedz się więcej o naszej szkolnej inicjatywie!</p>
+    </main>
+  );
+}
